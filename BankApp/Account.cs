4341f2970fdc323c88f1b53423f7bfc94735cb5d
jogit82 <jo.c.keller@gmail.com>
2017-09-24 11:54:43 -0700

    initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace BankApp
+{
+    public class Account
+    {
+        private static int lastAccountNumber = 0;
+        #region Properties
+        /// <summary>
+        /// This is about the account number
+        /// </summary>
+        public int AccountNumber
+        { get; }
+
+        /// <summary>
+        /// This is about the email address of the user
+        /// </summary>
+        public string EmailAddress
+        { get; set; }
+
+        public decimal Balance
+        { get; private set; }
+
+        public string AccountType
+        { get; set; }
+
+        public DateTime CreatedDate
+        { get; set; }
+
+		#endregion
+
+		#region  Constructors
+        public Account() 
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+		#endregion
+
+		#region Methods
+
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+		public decimal Withdraw(decimal amount)
+		{
+			Balance -= amount;
+            return Balance;
+		}
+
+		#endregion
+	}
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var account = new Account();
+            account.EmailAddress = "test@gmail.com";
+            account.AccountType = "Savings";
+
+            var newBalance = account.Deposit(100);
+            Console.WriteLine($"AccountNumber: {account.AccountNumber}, Email Address: {account.EmailAddress}, Balance: {account.Balance:C}, Account Type: {account.AccountType}");
+
+            var account2 = new Account();
+			account2.EmailAddress = "test2@gmail.com";
+			account2.AccountType = "Checking";
+            var newBalance2 = account2.Deposit(500);
+            Console.WriteLine($"AccountNumber: {account2.AccountNumber}, Email Address: {account2.EmailAddress}, Balance: {account2.Balance:C}, Account Type: {account2.AccountType}");
+        }
+    }
+}
